2895ceaf15f3fb6f8efd10333e80a816e58e18ec
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-24 08:31:24 +0500

    feat: express.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello, X-Auto!');
+const express = require('express');
+
+const app = express();
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`X-Auto is running at: https://${HOST}:${PORT}`);
+});
