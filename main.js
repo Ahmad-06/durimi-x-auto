fd6c5a6a9bac182442969821efdaefda174d8547
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-24 08:37:17 +0500

    feat: router.

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 
+const path = require('node:path');
 const express = require('express');
 
 const app = express();
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
-});
+app.use(require(path.join(__dirname, 'router', 'router')));
 
 app.listen(PORT, () => {
     console.log(`X-Auto is running at: https://${HOST}:${PORT}`);
