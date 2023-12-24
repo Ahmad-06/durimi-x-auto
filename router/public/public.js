d2f39db3e743a0368c7218b19c2fea2dffcb6912
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-24 08:40:56 +0500

    feat: home router.

--- router/public/public.js
+++ router/public/public.js
@@ -1,8 +1,7 @@
+const path = require('node:path');
 const express = require('express');
 
 const public = express.Router();
 module.exports = public;
 
-public.get('/', (req, res) => {
-    res.send('Hello, Public Router!');
-});
+public.use('/', require(path.join(__dirname, 'home', 'home')));
