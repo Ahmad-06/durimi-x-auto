b5ec9cf0cadaf1f9c36f589578d7bd23904732dd
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-27 11:43:31 +0500

    feat: tweets ui.

--- router/public/public.js
+++ router/public/public.js
@@ -5,4 +5,5 @@ const public = express.Router();
 module.exports = public;
 
 public.use('/', require(path.join(__dirname, 'home', 'home')));
+public.use('/tweets', require(path.join(__dirname, 'tweets', 'tweets')));
 public.use('/retweets', require(path.join(__dirname, 'retweets', 'retweets')));
