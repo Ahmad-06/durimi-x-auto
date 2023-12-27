b7cca8a4ddb81acdae2996637c302637065f038a
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-27 07:33:48 +0500

    feat: retweets table.

--- data/seedDB.js
+++ data/seedDB.js
@@ -31,6 +31,28 @@ const seedDB = async () => {
         }
     }
 
+    try {
+        const createRetweetsTable = `
+            CREATE TABLE IF NOT EXISTS retweets
+            (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                type NOT NULL,
+                link TEXT,
+                time TEXT,
+                timestamp TIMESTAMP,
+                priority INTEGER NOT NULL,
+                status TEXT
+            )
+        `;
+
+        await db.exec(createRetweetsTable);
+    } catch (err) {
+        if (err) {
+            await db.close();
+            return console.error('There was an error when trying to create the default retweets table: ', err);
+        }
+    }
+
     db.close();
 };
 
