049ab0b2801ff60fce69776ba22b223a0da9fab5
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-27 13:01:10 +0500

    feat: update retweet.

--- router/api/v1/retweets/retweets.js
+++ router/api/v1/retweets/retweets.js
@@ -6,4 +6,5 @@ module.exports = retweets;
 
 retweets.use('/', require(path.join(__dirname, 'routes', 'retweets.get')));
 retweets.use('/create', require(path.join(__dirname, 'routes', 'retweets.create')));
+retweets.use('/update', require(path.join(__dirname, 'routes', 'retweets.update')));
 retweets.use('/delete', require(path.join(__dirname, 'routes', 'retweets.delete')));
