f1b6324763fb77c803292b361fe4092e2abdf9b6
Ahmad Shahzad <ahmad@shahzad.dev>
2023-12-29 10:48:24 +0500

    feat: login route.

--- router/api/v1/misc/misc.js
+++ router/api/v1/misc/misc.js
@@ -4,4 +4,5 @@ const express = require('express');
 const misc = express.Router();
 module.exports = misc;
 
+misc.use('/login', require(path.join(__dirname, 'login', 'login')));
 misc.use('/indicator', require(path.join(__dirname, 'indicator', 'indicator')));
